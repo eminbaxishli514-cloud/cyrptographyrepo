3fcb07305e7aa4a795d2ded582bea55bad6ff86a
Emin Baxishli <eminbaxishli514@gmail.com>
2026-03-23 11:53:34 +0400

    Update affineencryption.py

--- affineencryption.py
+++ affineencryption.py
@@ -1,3 +1,4 @@
+#Encryption
 def finder(a, b):
     for i in range(len(a)):
         if a[i] == b:
@@ -22,3 +23,42 @@ def encrypt(plaintext, multiplier, shifter):
     else:
         return "For the inverse to exist, the number must be coprime to 26."
     return result
+
+
+#Decryption
+
+def inverse(a,m = 26):
+    for i in range(1,m):
+        if (i*a)%m ==1:
+            return i
+    return None
+
+def decrypt(ciphertext,multiplier,shifter,inverse_of_multiplier):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    if gcd(multiplier,26) == 1:
+        for i in ciphertext:
+            if i in letters:
+                y = finder(letters,i)
+                x = (inverse_of_multiplier*(y-shifter))%26
+                result = result + letters[x]
+            
+            else:
+                result = result + i
+    
+    return result
+
+#Usage
+plaintext = "hello world"
+multiplier = 5
+shifter = 8
+
+#Encryption
+ciphertext = encrypt(plaintext, multiplier, shifter)
+print("Encrypted text:", ciphertext)
+#Inverse
+inv = inverse(multiplier)
+
+#Decryption
+decrypted = decrypt(ciphertext, multiplier, shifter, inv)
+print("Decrypted text:", decrypted)
