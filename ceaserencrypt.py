95d566ef3e840f1f18eb4332efe9312a6c095006
Emin Baxishli <eminbaxishli514@gmail.com>
2026-03-23 11:49:03 +0400

    Update ceaserencrypt.py

--- ceaserencrypt.py
+++ ceaserencrypt.py
@@ -1,4 +1,4 @@
-#The source code of ceaser encryption algorithm
+#Encryption
 def finder(a,b):
     for i in range(len(a)):
         if a[i]==b:
@@ -15,6 +15,26 @@ def encrypt(text,key):
             result = result + i
     return result
 
-cleartext = input("Enter your message: ")
-key = int(input("Enter the key: "))
-print(encrypt(cleartext,key))
+#Decryption
+
+def decyrpt(ciphertext,key):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    for i in ciphertext:
+        if i in letters:
+            number = finder(letters,i)
+            result = result + letters[(number-key)%26]
+        else:
+            result = result + i
+    return result
+
+#Decryption
+#ciphertext = input("Enter the ciphertext: ")
+#key = int(input("Enter the key: "))
+#print(decyrpt(ciphertext,key))
+
+#Encryption
+#cleartext = input("Enter your message: ")
+#key = int(input("Enter the key: "))
+#print(encrypt(cleartext,key))
+
